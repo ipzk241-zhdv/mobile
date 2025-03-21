65c15f0b108dbc294e849f7a10e44be170a83cde
Жеребцов Дмитро Вікторович <ipzk241_zhdv@student.ztu.edu.ua>
2025-03-21 21:53:11 +0200

    profile screen

--- lab2/screens/ProfileScreen.tsx
+++ lab2/screens/ProfileScreen.tsx
@@ -1,37 +1,72 @@
 import React from "react";
-import { Button, View } from "react-native";
+import { Button, Text, TouchableOpacity, View } from "react-native";
 import styled from "styled-components/native";
+import ThemedView from "../components/ThemedView";
+import { SettingsButton } from "../components/SettingsButton";
 
 interface ProfileScreenProps {
-  toggleTheme: () => void;
-  isDarkMode: boolean;
+    toggleTheme: () => void;
+    isDarkMode: boolean;
 }
 
-// Themed контейнер
-const ThemedView = styled.View`
-  flex: 1;
-  background-color: ${(props) => props.theme.background};
-  padding: 20px;
-`;
-
-// Themed текст
-const ThemedText = styled.Text`
-  color: ${(props) => props.theme.text};
-  font-size: 20px;
-  margin-bottom: 20px;
-`;
-
 const ProfileScreen: React.FC<ProfileScreenProps> = ({ toggleTheme, isDarkMode }) => {
-  return (
-    <ThemedView>
-      <ThemedText>ProfileScreen</ThemedText>
-      <Button
-        title={isDarkMode ? "toggle theme" : "toggle theme"}
-        onPress={toggleTheme}
-        color={isDarkMode ? "#ccc" : "#333"}
-      />
-    </ThemedView>
-  );
+    return (
+        <ThemedView>
+            <AvatarContainer>
+                <AvatarWrapper>
+                    <Avatar
+                        source={{ uri: "https://images.dwncdn.net/images/t_app-icon-l/p/e8326516-9b2c-11e6-9634-00163ec9f5fa/2059490507/rust-logo" }}
+                    />
+                    <OnlineIndicator color={"green"} />
+                </AvatarWrapper>
+                <Name>Жеребцов Дмитро</Name>
+                <Name>ІПЗк-24-1</Name>
+            </AvatarContainer>
+            <TouchableOpacity>
+                <SettingsButton buttonName="Toggle Theme" onPress={toggleTheme}></SettingsButton>
+            </TouchableOpacity>
+            <SettingsButton buttonName="Settings"></SettingsButton>
+            <SettingsButton buttonName="Logout"></SettingsButton>
+        </ThemedView>
+    );
 };
+
+const AvatarContainer = styled.View`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 15px;
+    padding: 69px 0px 15px 0px;
+`;
+
+const AvatarWrapper = styled.View`
+    position: relative;
+    width: 98px;
+    height: 98px;
+    margin-bottom: 15px;
+`;
+
+const Avatar = styled.Image`
+    width: 98px;
+    height: 98px;
+    border-radius: 48px;
+`;
+
+const OnlineIndicator = styled.View<{ color: string }>`
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    width: 27px;
+    height: 27px;
+    border-radius: 14px;
+    border: 2px solid #1e1f2b;
+    background-color: ${({ color }) => color};
+`;
+
+const Name = styled.Text`
+    color: ${(props) => props.theme.text};
+    text-align: center;
+    font-size: 20px;
+`;
 
 export default ProfileScreen;
